521126ad3390f70b53cfc1d335af78f03cdd0928
EricKisee <erickiseemulwa@gmail.com>
2024-01-03 22:08:33 +0300

    Contact Me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import About from "@/components/About";
+import ContactMe from "@/components/ContactMe";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 import type { NextPage } from "next";
@@ -26,8 +28,14 @@ const Home: NextPage = ()=>{
       <section id="skills" className=" snap-center ">
         <Skills/>
       </section>
-      {/* Projects */}
-      {/* Contact Me */}
+      
+      <section id="projects" className=" snap-center ">
+        <Projects/>
+      </section>
+      
+      <section id="contact" className=" snap-center ">
+        <ContactMe/>
+      </section>
       
     </div>
   );
